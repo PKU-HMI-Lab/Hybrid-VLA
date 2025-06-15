49d243b86a7cf72cc470bb1b8876def630fa6b21
Zhuoyang Liu <144525970+ZhuoyangLiu2005@users.noreply.github.com>
2025-06-15 16:36:31 +0800

    add bridgev2

--- vla/datasets/rlds/oxe/configs.py
+++ vla/datasets/rlds/oxe/configs.py
@@ -82,6 +82,13 @@ OXE_DATASET_CONFIGS = {
         "state_encoding": StateEncoding.POS_QUAT,
         "action_encoding": ActionEncoding.EEF_POS,
     },
+    "bridgev2": {  # Version of Bridge V2 
+        "image_obs_keys": {"primary": "image_0", "secondary": "image_1", "wrist": None},
+        "depth_obs_keys": {"primary": None, "secondary": None, "wrist": None},
+        "state_obs_keys": ["EEF_state", None, "gripper_state"],
+        "state_encoding": StateEncoding.POS_EULER,
+        "action_encoding": ActionEncoding.EEF_POS,
+    },
     "kuka": {
         "image_obs_keys": {"primary": "image", "secondary": None, "wrist": None},
         "depth_obs_keys": {"primary": None, "secondary": None, "wrist": None},
